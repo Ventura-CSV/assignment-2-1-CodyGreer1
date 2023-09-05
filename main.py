a8ba8b09a13ae0fadfb7897201750e1127b49422
Kyuwoong Lee <kwooler@gmail.com>
2023-09-05 11:22:09 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,21 @@
 def main():
-    """
+    
     ##################################################
-    m_perc = 40
-    f_perc = 60
-    m_students = 40
-    f_students = 60
+    # m_perc = 40
+    # f_perc = 60
+    
+    m_students = int(input())
+    f_students = int(input())
+    
     total_students = m_students + f_students
+    m_perc = m_students / total_students
+    f_perc = f_students / total_students
+    
     print (total_students)
     print (m_perc)
     print (f_perc)
     ##################################################
-    """
+    
 
     """
     ########################################
